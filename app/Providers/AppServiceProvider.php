2da83208c2492fe404329345b0e85701571f8ff3
tqt97 <kutuanonline199@gmail.com>
2025-09-09 20:29:24 +0700

    Add macro reading time

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Str::macro('readingTimeInMinutes', function (string $subject, $wordsPerMinute = 200) {
+            $readTime = intval(ceil(Str::wordCount(strip_tags($subject)) / $wordsPerMinute));
+            $wordCounts = Str::wordCount(strip_tags($subject));
+            $pluralWordLabel = Str::plural('word', $wordCounts);
+            $pluralTimeLabel = Str::plural('minute', $readTime);
+
+            return $wordCounts . ' ' . $pluralWordLabel . ' - ' . $readTime . ' ' . $pluralTimeLabel . ' to reading';
+        });
     }
 }
